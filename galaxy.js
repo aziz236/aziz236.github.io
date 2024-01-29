d4817c1678e3a90ab6fb9437ab94fad9bbac0b19
aziz236 <67267252+aziz236@users.noreply.github.com>
2024-01-29 19:37:23 +0100

    Add files via upload

---
+++ galaxy.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Ajoutez ici le code pour envoyer le formulaire, par exemple avec fetch()
+    alert("Formulaire soumis avec succès!");
+});
---
+++ index.js
@@ -0,0 +1,6 @@
+<script>
+    function toggleParagraph() {
+        var paragraph = document.getElementById("welcome-paragraph");
+        paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
+    }
+</script>
